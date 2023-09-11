7f7a50351d0e75b4fe62218876575637c9f6dfe3
Preetam Sharma <preetamxharma@gmail.com>
2023-09-11 20:07:54 +0530

    created top header

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,39 +1,107 @@
 package com.example.tipcalculator
 
+import android.graphics.Paint.Style
+import android.hardware.camera2.TotalCaptureResult
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TipCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+            MyApp {
+                TopHeader(1.0)
             }
         }
     }
 }
 
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    TipCalculatorTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier,
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
+fun TopHeader(totalPerPerson: Double = 0.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .padding(all = 24.dp)
+            .clip(CircleShape.copy(all = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7),
+
+        ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total Per Person",
+                style = TextStyle(
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 24.sp
+                ),
+            )
+            Text(
+                text = "$$total",
+                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 40.sp),
+            )
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+@Composable
 fun GreetingPreview() {
     TipCalculatorTheme {
-
+        MyApp {
+            Text(text = "hello there")
+        }
     }
 }
