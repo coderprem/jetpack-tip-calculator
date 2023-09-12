68dc9fd62335fb0d984983c8bb859251185cc502
Preetam Sharma <preetamxharma@gmail.com>
2023-09-12 12:56:39 +0530

    added components.kt file

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.tipcalculator
 
-import android.graphics.Paint.Style
-import android.hardware.camera2.TotalCaptureResult
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,17 +10,25 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AttachMoney
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -32,6 +38,10 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,7 +52,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                TopHeader(1.0)
+                Column {
+                    TopHeader(1.0)
+                    MainContent()
+                }
             }
         }
     }
@@ -53,8 +66,7 @@ fun MyApp(content: @Composable () -> Unit) {
     TipCalculatorTheme {
         // A surface container using the 'background' color from the theme
         Surface(
-            modifier = Modifier,
-            color = MaterialTheme.colorScheme.background
+            modifier = Modifier, color = MaterialTheme.colorScheme.background
         ) {
             content()
         }
@@ -84,13 +96,58 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
             Text(
                 text = "Total Per Person",
                 style = TextStyle(
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 24.sp
+                    fontWeight = FontWeight.SemiBold, fontSize = 24.sp
                 ),
             )
             Text(
                 text = "$$total",
                 style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 40.sp),
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    Surface(
+        modifier = Modifier
+            .padding(horizontal = 18.dp, vertical = 6.dp)
+            .fillMaxWidth()
+            .height(300.dp),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        color = Color(0xFFFFFFFF),
+        border = BorderStroke(1.dp, color = Color.Gray)
+    ) {
+        val value by remember {
+            mutableStateOf("value")
+        }
+        Column {
+            var textValue by remember {
+                mutableStateOf("")
+            }
+            OutlinedTextField(
+                value = textValue,
+                onValueChange = { textValue = it },
+                placeholder = {
+                    Text(text = "Enter Bill")
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Outlined.AttachMoney,
+                        contentDescription = "Dollar sign",
+                        tint = Color.Gray
+                    )
+                },
+                textStyle = TextStyle(textAlign = TextAlign.Left),
+                keyboardOptions = KeyboardOptions(autoCorrect = false,
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done),
+                singleLine = true,
             )
         }
     }
---
+++ app/src/main/java/com/example/tipcalculator/components/Components.kt
@@ -0,0 +1,56 @@
+package com.example.tipcalculator.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AttachMoney
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.w3c.dom.Text
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputField(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    labelId: String,
+    isSingleLine: Boolean,
+    isEnable: Boolean,
+    keyBoardType: KeyboardType = KeyboardType.Number,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+    OutlinedTextField(
+        modifier = modifier.padding(all = 10.dp),
+        value = valueState.value,
+        onValueChange = { valueState.value = it },
+        label = { Text(text = labelId) },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Outlined.AttachMoney,
+                contentDescription = "Money sign"
+            )
+        },
+        singleLine = isSingleLine,
+        textStyle = TextStyle(fontSize = 18.sp, color = MaterialTheme.colorScheme.onBackground),
+        enabled = isEnable,
+        keyboardOptions = KeyboardOptions(keyboardType = keyBoardType, imeAction = imeAction),
+        keyboardActions = onAction
+    )
+}
